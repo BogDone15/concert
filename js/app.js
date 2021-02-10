35deb343e7934016c52302e0ad4a6bae254ffbdc
BogDone <buselb2@gmail.com>
2021-02-10 16:01:54 +0200

    add proj

--- js/app.js
+++ js/app.js
@@ -1,28 +1,40 @@
 const priceWrap = document.querySelector('.main__price-wrap');
 const hidePrice = document.querySelector('.hide__price');
-const ticketsWrap = document.querySelector('.tickets__wrap');
 
 priceWrap.addEventListener('click', () => {
  hidePrice.classList.toggle('active');
 });
 
-class Tickets {
- async getTickets() {
+let cart = [];
+let seatsDOM = [];
+
+class Seats {
+
+ async getSeats() {
   try {
-   let result = await fetch('tickets.json');
+   let result = await fetch('seats.json');
    let data = await result.json();
-   let tickets = data.placesMap;
-   tickets = tickets.map(ticket => {
-    const { id } = ticket.sys;
-    const { state } = ticket.free;
-    const { priceGroup } = ticket.group;
-    const { imagePosition, humanizedPosition } = ticket.fields;
-    // const { imagePosition } = ticket.fields.imagePosition;
-    // const { humanizedPosition } = ticket.fields.humanizedPosition;
-    return { id, state, priceGroup, imagePosition, humanizedPosition };
+   let seats = data.placesMap;
+   seats = seats.map(ticket => {
+    const {
+     id,
+     state,
+     priceGroup,
+     imagePosition,
+     humanizedPosition,
+     price
+    } = ticket;
+    return {
+     id,
+     state,
+     priceGroup,
+     imagePosition,
+     humanizedPosition,
+     price
+    };
    });
-   return tickets;
-   
+   return seats;
+
   } catch (error) {
    console.log(error);
   }
@@ -30,33 +42,126 @@ class Tickets {
 }
 
 class UI {
- displayTickets(tickets) {
-  let result = "";
-  tickets.forEach(ticket => {
-   result += `
-   <div class="tickets__wrap-block-first">
-   <div style="background-color: ${ticket.state = booked ? gray : green};top: ${imagePosition.x}px;left: ${imagePosition.y}px" class="tickets__item" data-id=${ticket.id}></div>
-   <div style="background-color: ${ticket.state = booked ? gray : green};top: ${imagePosition.x}px;left: ${imagePosition.y}px" class="tickets__item" data-id=${ticket.id}></div>
-   <div style="background-color: ${ticket.state = booked ? gray : green};top: ${imagePosition.x}px;left: ${imagePosition.y}px" class="tickets__item" data-id=${ticket.id}></div>
-   <div style="background-color: ${ticket.state = booked ? gray : green};top: ${imagePosition.x}px;left: ${imagePosition.y}px" class="tickets__item" data-id=${ticket.id}></div>
-   <div style="background-color: ${ticket.state = booked ? gray : green};top: ${imagePosition.x}px;left: ${imagePosition.y}px" class="tickets__item" data-id=${ticket.id}></div>
-   <div style="background-color: ${ticket.state = booked ? gray : green};top: ${imagePosition.x}px;left: ${imagePosition.y}px" class="tickets__item" data-id=${ticket.id}></div>
-  </div>
-   `
-   
-  });
-  ticketsWrap.innerHTML = result;
+ constructor({
+  seatsWrap,
+  ticketsWrap,
+  mainPayment,
+  ticketItem
+ }) {
+  this.elements = {
+   seatsWrap: document.querySelector(seatsWrap),
+   ticketsWrap: document.querySelector(ticketsWrap),
+   mainPayment: document.querySelector(mainPayment),
+   ticketItem: document.querySelector(ticketItem),
+  }
  }
 
+ displaySeats(seats) {
+  let result = "";
+  seats.forEach(seat => {
+   result += `
+   <div style="background-color: ${seat.state == 'booked' ? 'gray' : 'green'};left: ${seat.imagePosition.x}px;top: ${seat.imagePosition.y}px" class="tickets__item" data-state=${seat.state} data-id=${seat.id}></div>
+   `
+  });
+
+  this.elements.seatsWrap.insertAdjacentHTML('afterbegin', result);
+ }
+
+ getSeat() {
+  const seats = [...document.querySelectorAll('.tickets__item')];
+  seatsDOM = seats;
+  seats.forEach(seat => {
+   let id = seat.dataset.id;
+   seat.addEventListener('click', e => {
+
+    const target = e.target;
+    target.style.backgroundColor = '#00FF00';
+    target.dataset.state == 'booked';
+    if (target.dataset.state == 'booked') {
+     target.style.backgroundColor = 'gray';
+    }
+
+    let cartItem = {...Storage.getSeats(id)};
+    
+    cart = [...cart, cartItem];
+
+    Storage.saveCart(cart);
+
+    this.addTicket(cartItem);
+
+    this.showCart();
+
+   });
+   seat.addEventListener('mouseenter', e => {
+    if (e.target.dataset.state == 'available') {
+     e.target.style.transform = 'scale(1.1)';
+    }
+   });
+
+   seat.addEventListener('mouseleave', e => {
+    if (e.target.dataset.state == 'available') {
+     e.target.style.transform = 'scale(1)';
+    }
+   });
+  });
+ }
+
+ addTicket(item) {
+  const div = document.createElement('div');
+  div.classList.add('payment-ticket-item');
+  div.innerHTML = `
+  <div class="payment-ticket-item-title">
+        ПАРТЕР
+       </div>
+       <div class="payment-ticket-item-info">
+       <div class="payment-ticket-item-info-wrap">
+        <span class="payment-ticket-item-place">Место: ${item.humanizedPosition.place}</span>
+        <span class="payment-ticket-item-row">Ряд: ${item.humanizedPosition.row}</span>
+       </div>
+        <div class="payment-ticket-item-info-price">
+         ${item.price} грн
+        </div>
+       </div>
+  `
+  this.elements.ticketsWrap.appendChild(div);
+ }
+
+ showCart() {
+   this.elements.mainPayment.style.display = 'none';   
+   this.elements.ticketItem.style.transform = 'translateX(0)';
+ }
 }
 
-const tickets = new Tickets();
-const ui = new UI();
+class Storage {
+ static saveSeats(seats) {
+  localStorage.setItem('seats', JSON.stringify(seats));
+ }
 
-tickets.getTickets().then(tickets => {
- ui.displayTickets(tickets);
- // console.log(tickets);
-})
+ static getSeats(id) {
+  let seats = JSON.parse(localStorage.getItem('seats'));
+  return seats.find(seat => seat.id == id);
+ }
+
+ static saveCart(cart) {
+  localStorage.setItem('cart', JSON.stringify(cart));
+ }
+}
+
+const seats = new Seats();
+const ui = new UI({
+ seatsWrap: '.tickets__wrap-block-first',
+ ticketsWrap: '.payment-ticket-item-wrap',
+ mainPayment: '.main__payment-reminders',
+ ticketItem: '.payment-ticket-item',
+});
+
+
+seats.getSeats().then(seats => {
+ ui.displaySeats(seats);
+ Storage.saveSeats(seats);
+}).then(() => {
+ ui.getSeat();
+});
 
 
 
